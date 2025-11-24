4a174799d1bdbc6bcd7c756e1041ac0e0c675a0d
Saul Hernandez <shern400@stu.southtexascollege.edu>
2025-11-24 00:05:22 -0600

    fixed errors with plugins

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -9,12 +9,14 @@ const props = defineProps({
 
 <template>
     <div class="bg-white shadow rounded-xl">
-        <router-link :to="`/meal/${props.meal.idMeal}`">
+        <router-link :to="{}">
             <img :src="props.meal.strMealThumb" class="rounded-t-xl h-48 object-cover w-full"/>
         </router-link>
         <div class="p-3">
             <h3 class="font-bold">{{ props.meal.strMeal }}</h3>
             <p class="mb-4 line-clamp-3">{{ props.meal.strInstructions }}</p>
         </div>
+        
     </div>
+
 </template>
